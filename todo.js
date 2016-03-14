9e5b0fe1e623d14b5982384388efa2e1693676b5
Dane Guevara <dane.guevara@cerner.com>
2016-03-14 13:32:18 -0500

    Move REST callbacks into todo model.

--- todo.js
+++ todo.js
@@ -2,8 +2,43 @@
 
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('Todo', {
+var Todo = mongoose.model('Todo', {
   text: String,
   done: Boolean,
   archived: Boolean
 });
+
+module.exports = {
+
+    // return all todos
+    get: function (req, res) {
+      Todo.find(function (err, todos) {
+        res.send(err || todos);
+      });
+    },
+
+    // create a todo
+    post: function (req, res) {
+      Todo.create({
+        text: req.body.text,
+        done: false,
+        archived: false
+      }, function (err, created) {
+        res.send(err || created);
+      });
+    },
+
+    // update a todo
+    put: function (req, res) {
+      Todo.update({_id: req.params.id}, req.body, function (err, updated) {
+        res.send(err || updated);
+      });
+    },
+
+    // remove a todo
+    delete: function (req, res) {
+      Todo.remove({_id: req.params.id}, function (err, deleted) {
+        res.send(err || deleted);
+      });
+    }
+};
